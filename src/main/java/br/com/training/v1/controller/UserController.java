c5b9fbb5b83fb64007bda4c20c120e06f8ed8bbf
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-25 15:38:18 -0300

    update ResponseEntity status

--- src/main/java/br/com/training/v1/controller/UserController.java
+++ src/main/java/br/com/training/v1/controller/UserController.java
@@ -20,51 +20,44 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
         UserResponse newUser = this.userService.salvar(userRequest);
-        return ResponseEntity.ok().body(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
     @PutMapping(value = "/{cpf}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<UserResponse> updateUser(@PathVariable String cpf, @Valid @RequestBody UserRequest userRequest) {
         UserResponse updateUser = this.userService.atualizar(cpf, userRequest);
-        return ResponseEntity.ok().body(updateUser);
+        return ResponseEntity.status(HttpStatus.OK).body(updateUser);
     }
 
     @GetMapping(value = "/{id}")
-    @ResponseStatus(HttpStatus.OK)
+
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
         UserResponse userResponse = this.userService.findById(id);
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
     @GetMapping(value = "/cpf/{cpf}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<UserResponse> getUserByCpf(@PathVariable String cpf) {
         UserResponse userResponse = this.userService.findByCpf(cpf);
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
     @GetMapping(value = "/email/{email}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<UserResponse> getUserByEmail(@PathVariable String email) {
         UserResponse userResponse = this.userService.findByEmail(email);
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<UserResponse>> getAllUser() {
         List<UserResponse> userResponse = this.userService.findAll();
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponse);
     }
 
     @DeleteMapping(value = "/{cpf}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<Void> deleteUser(@PathVariable String cpf) {
+    public ResponseEntity<?> deleteUser(@PathVariable String cpf) {
         this.userService.excluir(cpf);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-
 }
